9113050f7ecb81171b06750bcc23f0dfd77fe682
Andrey Kashapov <kashapovandrey@gmail.com>
2018-11-28 14:19:42 +0300

    CSS Transitions and Animations Limitations

--- src/App.js
+++ src/App.js
@@ -8,23 +8,25 @@ import List from "./components/List/List";
 class App extends Component {
   state = {
     modalIsOpen: false
-  }
+  };
 
   showModal = () => {
     this.setState({ modalIsOpen: true });
-  }
+  };
 
   closeModal = () => {
     this.setState({ modalIsOpen: false });
-  }
+  };
 
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        <Backdrop show={this.state.modalIsOpen} />
-        <button className="Button" onClick={this.showModal}>Open Modal</button>
+        {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> : null}
+        {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null}
+        <button className="Button" onClick={this.showModal}>
+          Open Modal
+        </button>
         <h3>Animating Lists</h3>
         <List />
       </div>
